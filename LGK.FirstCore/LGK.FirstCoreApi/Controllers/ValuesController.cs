aab79a5c593f384f7574b0f834021f99bbf02594
1564836981@qq.com <1564836981@qq.com>
2019-09-10 09:10:17 +0800

    4

--- LGK.FirstCore/LGK.FirstCoreApi/Controllers/ValuesController.cs
+++ LGK.FirstCore/LGK.FirstCoreApi/Controllers/ValuesController.cs
@@ -29,24 +29,24 @@ namespace LGK.FirstCoreApi.Controllers
 
 
 
-        public ActionResult<IEnumerable<string>> Get()
-        {
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
 
-            var user = new Userss
-            {
-                UserName = "test"
-            };
-            var i = userRepository.Insert(user);
+        //    var user = new Userss
+        //    {
+        //        UserName = "test"
+        //    };
+        //    var i = userRepository.Insert(user);
 
 
-            var tmpUser = userRepository.GetModelById(4);
-            tmpUser.UserName = "test222";
-            i = userRepository.Update(tmpUser);
+        //    var tmpUser = userRepository.GetModelById(4);
+        //    tmpUser.UserName = "test222";
+        //    i = userRepository.Update(tmpUser);
 
-            i = userRepository.Delete(4);
+        //    i = userRepository.Delete(4);
 
-            return new string[] { "value1", "value2" };
-        }
+        //    return new string[] { "value1", "value2" };
+        
     }
 }
     
